73daf12868ff417d3f06daefc631a31cdc136b83
titosilva <jt.mat@hotmail.com>
2025-01-26 09:41:23 -0300

    renaming DLHider

--- crypto/homomorphic_hiding/dlhh/dlhh.go
+++ crypto/homomorphic_hiding/dlhh/dlhh.go
@@ -5,20 +5,20 @@ import (
 	"github.com/titosilva/pdpr-go/math/nmod"
 )
 
-type DLHidder struct {
+type DLHider struct {
 	dlg *dl.DiscreteLogGroup
 }
 
-func New(dlg *dl.DiscreteLogGroup) *DLHidder {
-	return &DLHidder{dlg}
+func New(dlg *dl.DiscreteLogGroup) *DLHider {
+	return &DLHider{dlg}
 }
 
-func (dlh *DLHidder) Hide(data []byte) []byte {
+func (dlh *DLHider) Hide(data []byte) []byte {
 	r := dlh.dlg.Gen.ExpBytes(data)
 	return r.Bytes()
 }
 
-func (dlh *DLHidder) Combine(hidden1 []byte, hidden2 []byte) []byte {
+func (dlh *DLHider) Combine(hidden1 []byte, hidden2 []byte) []byte {
 	h1Nat := nmod.NewFromBigEndianBytes(hidden1, dlh.dlg.Mod)
 	h2Nat := nmod.NewFromBigEndianBytes(hidden2, dlh.dlg.Mod)
 	r := h1Nat.Mul(h2Nat)
@@ -26,7 +26,7 @@ func (dlh *DLHidder) Combine(hidden1 []byte, hidden2 []byte) []byte {
 	return r.Bytes()
 }
 
-func (dlh *DLHidder) Verify(data []byte, hidden []byte) bool {
+func (dlh *DLHider) Verify(data []byte, hidden []byte) bool {
 	hNat := nmod.NewFromBigEndianBytes(hidden, dlh.dlg.Mod)
 	dNat := nmod.NewFromBigEndianBytes(data, dlh.dlg.Mod)
 	r := dlh.dlg.Gen.Exp(dNat)
